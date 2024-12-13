2f66967b0f6e2a4846526f31c585743f6d9f21af
DariusKando <144209531+DariusKando@users.noreply.github.com>
2024-12-13 08:44:12 +0100

    database fix 1.5

--- erettsegizzunk.sql
+++ erettsegizzunk.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2024. Dec 13. 07:33
--- Kiszolgáló verziója: 10.4.28-MariaDB
--- PHP verzió: 8.2.4
+-- Létrehozás ideje: 2024. Dec 13. 08:43
+-- Kiszolgáló verziója: 10.4.32-MariaDB
+-- PHP verzió: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -62,6 +62,27 @@ CREATE TABLE `feladatok_tema` (
   `feladatokId` int(11) NOT NULL,
   `temaId` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `permission`
+--
+
+CREATE TABLE `permission` (
+  `Id` int(11) NOT NULL,
+  `Level` int(1) NOT NULL,
+  `Name` varchar(32) NOT NULL,
+  `Description` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+
+--
+-- A tábla adatainak kiíratása `permission`
+--
+
+INSERT INTO `permission` (`Id`, `Level`, `Name`, `Description`) VALUES
+(1, 0, 'Luzer', 'Webes regisztráció felhasználó'),
+(2, 9, 'Administrator', 'Rendszergazda');
 
 -- --------------------------------------------------------
 
@@ -133,6 +154,31 @@ INSERT INTO `tipus` (`id`, `nev`) VALUES
 (2, 'checkbox'),
 (3, 'textbox');
 
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `user`
+--
+
+CREATE TABLE `user` (
+  `Id` int(11) NOT NULL,
+  `LoginName` varchar(16) NOT NULL,
+  `HASH` varchar(64) NOT NULL,
+  `SALT` varchar(64) NOT NULL,
+  `Name` varchar(64) NOT NULL,
+  `PermissionId` int(11) NOT NULL,
+  `Active` tinyint(1) NOT NULL,
+  `Email` varchar(64) NOT NULL,
+  `ProfilePicturePath` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+
+--
+-- A tábla adatainak kiíratása `user`
+--
+
+INSERT INTO `user` (`Id`, `LoginName`, `HASH`, `SALT`, `Name`, `PermissionId`, `Active`, `Email`, `ProfilePicturePath`) VALUES
+(1, 'kerenyir', 'd5fe0e517520122f1ab363b6b7ee9ae616e7ad393693ef00d81a7f287a79931a', 'Gm63C4jiWnYvfZfiKUu2cu8AHPNDj8NoHhtQn88yiJhyOunBNSd7tRoWo5wwqg9X', 'Kerényi Róbert', 2, 1, 'kerenyir@kkszki.hu', 'img\\kerenyir.jpg');
+
 --
 -- Indexek a kiírt táblákhoz
 --
@@ -152,6 +198,14 @@ ALTER TABLE `feladatok`
 ALTER TABLE `feladatok_tema`
   ADD PRIMARY KEY (`feladatokId`,`temaId`),
   ADD KEY `temaId` (`temaId`);
+
+--
+-- A tábla indexei `permission`
+--
+ALTER TABLE `permission`
+  ADD PRIMARY KEY (`Id`),
+  ADD UNIQUE KEY `Szint` (`Level`),
+  ADD UNIQUE KEY `Nev` (`Name`);
 
 --
 -- A tábla indexei `szint`
@@ -178,6 +232,15 @@ ALTER TABLE `tipus`
   ADD PRIMARY KEY (`id`);
 
 --
+-- A tábla indexei `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`Id`),
+  ADD UNIQUE KEY `LoginNev` (`LoginName`),
+  ADD UNIQUE KEY `Email` (`Email`),
+  ADD KEY `Jog` (`PermissionId`);
+
+--
 -- A kiírt táblák AUTO_INCREMENT értéke
 --
 
@@ -186,6 +249,12 @@ ALTER TABLE `tipus`
 --
 ALTER TABLE `feladatok`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT a táblához `permission`
+--
+ALTER TABLE `permission`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT a táblához `szint`
@@ -212,6 +281,12 @@ ALTER TABLE `tipus`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
+-- AUTO_INCREMENT a táblához `user`
+--
+ALTER TABLE `user`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
 -- Megkötések a kiírt táblákhoz
 --
 
@@ -229,6 +304,12 @@ ALTER TABLE `feladatok`
 ALTER TABLE `feladatok_tema`
   ADD CONSTRAINT `feladatok_tema_ibfk_1` FOREIGN KEY (`feladatokId`) REFERENCES `feladatok` (`id`),
   ADD CONSTRAINT `feladatok_tema_ibfk_2` FOREIGN KEY (`temaId`) REFERENCES `tema` (`id`);
+
+--
+-- Megkötések a táblához `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`PermissionId`) REFERENCES `permission` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
